164b9208ec950f0e8e20032af14a3eb713751f43
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2026-01-26 18:34:24 -0300

    refactor: update import path for frontend localhost constant in environment validation schema
    - Change import of DEFAULT_FRONTEND_LOCALHOST from constants to globalConfig for better organization and clarity in environment validation.

--- back-end/src/utils/validations/envs.schemas.ts
+++ back-end/src/utils/validations/envs.schemas.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 import { ENodeEnvs } from '../../types/envs'
-import { DEFAULT_FRONTEND_LOCALHOST } from '../constants'
+import { DEFAULT_FRONTEND_LOCALHOST } from '../globalConfig'
 
 export const globalEnvsSchema = z.object({
     NODE_ENV: z.enum([ENodeEnvs.DEVELOPMENT, ENodeEnvs.PRODUCTION, ENodeEnvs.TEST]),
